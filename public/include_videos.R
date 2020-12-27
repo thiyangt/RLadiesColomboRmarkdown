97664f8e3a2c52eba054b0ca0bc3653b0a87fef7
thiyangt <thiyanga.talagala@monash.edu>
2020-12-27 15:27:55 +0530

    update slides

---
+++ public/include_videos.R
@@ -0,0 +1,10 @@
+## video
+    
+<video width="100%" height="100%">
+<source src="static/rmarkdown_rladies/media/sc1.mp4" type="video/mp4">
+</video>
+
+## iframe
+        
+<iframe width="560" height="315" src="media/sc1.mp4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
